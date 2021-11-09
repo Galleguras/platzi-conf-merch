ee76651caabcf65035d1a0c4fd4eb3aa7bcc1d7c
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-11-09 16:30:01 +0100

    29 Implementando useContext en Platzi Conf Merch en Curso Profesional de React Hooks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import Checkout from '../containers/Checkout';
-import '../styles/components/Header.css';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AppContext from '../context/AppContext';
+import '../styles/components/Header.css';
 const Header = () => {
+  const { state } = useContext(AppContext);
+  const { cart } = state;
   return (
     <div className="Header">
-      <Link to="/">
-        <h1 className="Header-title">PlatziConf Merch</h1>
-      </Link>
+      <h1 className="Header-title">
+        <Link to="/">PlatziConf Merch</Link>
+      </h1>
       <div className="Header-checkout">
         <Link to="/checkout">
           <i className="fas fa-shopping-basket" />
         </Link>
+        {cart.length > 0 && <div className="Header-alert">{cart.length}</div>}
       </div>
     </div>
   );
